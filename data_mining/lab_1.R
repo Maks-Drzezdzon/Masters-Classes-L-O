7a724c1ca416863e66703d83f81c0b527f63580e
maksthorn <maksdrzezdzon@gmail.com>
2020-10-12 19:56:19 +0100

    done with lab, review later

--- data_mining/lab_1.R
+++ data_mining/lab_1.R
@@ -12,3 +12,20 @@ data_1 = htmlToText("http://oralytics.com")
 data_3 = htmlToText("http://www.tudublin.ie")
 
 data_merge = c(data_3, data_1)
+
+txt_corpus = Corpus(VectorSource(data_merge))
+summary(txt_corpus)
+tm_map <- tm_map (txt_corpus, stripWhitespace) # remove white space
+tm_map <- tm_map (tm_map, removePunctuation) # remove punctuations
+tm_map <- tm_map (tm_map, removeNumbers) # to remove numbers
+tm_map <- tm_map (tm_map, removeWords, stopwords("english")) # to remove stop words(like 'as' 'the' etc..)
+tm_map <- tm_map (tm_map, stemDocument)
+tm_map <- tm_map (tm_map, removeWords, c("work", "use", "java", "new", "support"))
+inspect(tm_map)
+Matrix <- TermDocumentMatrix(tm_map) # terms in rows
+matrix_c <- as.matrix (Matrix)
+freq <- sort (rowSums (matrix_c)) # frequency count of the data
+freq #view the words and their frequencies
+tmdata <- data.frame (words=names(freq), freq)
+wordcloud (tmdata$words, tmdata$freq, max.words=100, min.freq=3, scale=c(7,.5), random.order=FALSE, colors=brewer.pal(8, "Dark2"))
+
--- data_mining/lab_1_p2.py
+++ data_mining/lab_1_p2.py
@@ -4,7 +4,13 @@ from pdfminer.converter import TextConverter
 from pdfminer.pdfinterp import PDFPageInterpreter
 from pdfminer.pdfinterp import PDFResourceManger
 from pdfminer.pdfpage import PDFPageInterpreter
+from nltk.tokenize import sent_tokenize, word_tokenize
 
+# tokenize - standardize text - remove stop words - remove punctuation - remove word extensions (stemming) - aggregate data
 
+#19 min
+pdfFile = wkDir+'PGManifesto16_2.pdf'
 
+words = word_tokenize(text)
+print(words)
 
