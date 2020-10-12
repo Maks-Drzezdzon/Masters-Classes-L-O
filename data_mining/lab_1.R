85305399a14870a09b9605a59242e9f94107e9e7
maksthorn <maksdrzezdzon@gmail.com>
2020-10-12 19:25:19 +0100

    setting up lab

--- data_mining/lab_1.R
+++ data_mining/lab_1.R
@@ -5,3 +5,10 @@ library(wordcloud)
 library(SnowballC)
 
 source("htmlToText.R")
+
+
+data_1 = htmlToText("http://oralytics.com")
+# data_2 = htmlToText("http://www.rte.ie/news") # its dead
+data_3 = htmlToText("http://www.tudublin.ie")
+
+data_merge = c(data_3, data_1)
---
+++ data_mining/lab_1_p2.py
@@ -0,0 +1,10 @@
+import io import pdfminer
+from pprint import pprint
+from pdfminer.converter import TextConverter
+from pdfminer.pdfinterp import PDFPageInterpreter
+from pdfminer.pdfinterp import PDFResourceManger
+from pdfminer.pdfpage import PDFPageInterpreter
+
+
+
+
