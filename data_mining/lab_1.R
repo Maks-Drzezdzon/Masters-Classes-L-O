3211f5b4c1ad1fd8b5fb29910ce8a974859456e6
maksthorn <maksdrzezdzon@gmail.com>
2020-10-14 19:42:36 +0100

    setting up lab

--- data_mining/lab_1.R
+++ data_mining/lab_1.R
@@ -8,7 +8,7 @@ source("htmlToText.R")
 
 
 data_1 = htmlToText("http://oralytics.com")
-# data_2 = htmlToText("http://www.rte.ie/news") # its dead
+data_2 = htmlToText("http://www.rte.ie/news") # its dead
 data_3 = htmlToText("http://www.tudublin.ie")
 
 data_merge = c(data_3, data_1)
