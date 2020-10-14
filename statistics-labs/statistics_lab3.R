53fbc19c62edee25caab026d0ceb453a28f836d6
maksthorn <maksdrzezdzon@gmail.com>
2020-10-14 20:59:30 +0100

    lab work and portfolio

--- statistics-labs/statistics_lab3.R
+++ statistics-labs/statistics_lab3.R
@@ -4,11 +4,18 @@ survey = read.table("stats_lab3/survey.dat",sep=";",header=TRUE)
 cor.test(data, data, method = "spearman")
 
 # ToDo for portfolio
+# parametric tests
 # skewness and kurtosis z-values
-# --- -/+ 1.96 
+# the closer to 0 the better
+
+# skewness / std error
+# kurtosis / std error
+# should be between -/+ 1.96 
 
 # shapiro-wilk test p-value
-# above 0.05
+# null-H is that the data is normally distributed 
+# so above 0.05, data is approx distributed
+# it is rejected if it is below 0.05, is not approx distributed
 
 # visual tests
 # histogram, normal Q-Q plots and box plots
