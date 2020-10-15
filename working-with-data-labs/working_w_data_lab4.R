4deadf8e857f0512809caf8540614620feb2c8f1
maksthorn <maksdrzezdzon@gmail.com>
2020-10-15 20:06:07 +0100

    small additions

--- working-with-data-labs/working_w_data_lab4.R
+++ working-with-data-labs/working_w_data_lab4.R
@@ -23,3 +23,21 @@ fivenum(df$age)
 # Q75% - Q25%
 IQR(df$age)
 
+# data viz
+# grammar of graphics book
+library('ggplot2')
+?mpg
+# 6 points dont follow the trend 
+# red dots
+qplot(data = mpg, x = displ, y = hwy, color = I('red'))
+# well.. size
+qplot(data = mpg, x = displ, y = hwy, size = I(0.25), geom = 'smooth')
+# transparency 
+qplot(data = mpg, x = displ, y = hwy, alpha = I(0.1))
+# x over dot
+qplot(data = mpg, x = displ, y = hwy, shape = I(4))
+# color dots by its category and div by category in drv
+qplot(data = mpg, x = displ, y = hwy, color = class) + facet_grid(drv ~ .)
+
+
+
