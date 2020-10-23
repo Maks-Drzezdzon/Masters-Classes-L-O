1b1c73986000dc0520b9555640156a953dcda92e
maksthorn <maksdrzezdzon@gmail.com>
2020-10-23 18:52:37 +0100

    struct code

--- assignments/working_w_data/data_wraggling_assignment.R
+++ assignments/working_w_data/data_wraggling_assignment.R
@@ -4,16 +4,21 @@ library(data.table)
 
 # file is small enough to hold in memory ~2gigs
 # use read_csv for optimized load speed
+# https://www.kaggle.com/PROPPG-PPG/hourly-weather-surface-brazil-southeast-region
 df = read_csv('weather-data.csv')
 # inspect df data types and cols
 spec(df)
+
 # rename cols to be more descriptive 
-# ID = wsid, w_station_name = wsnm,
-names_old = c(colnames(df))
-names_new = c("ID", "w_station_name", "elevation", "lat", "long", "station_num", "city", "province", "observation_date_time", "observation_date", "year", "month", "day", "hour", "precipitation_last_hr_ml", "air_pressure_hr_hPa", "max_air_pressure_hr_hPa", "min_air_pressure_hr_hPa", "solar_radiation", "air_temprature", "dew_temp", "max_temp_hr", "max_dew_temp", "min_temp_hr", "min_dew_temp", "relative_humidity", "max_relative_humidity", "min_relative_humidity", "wind_speed", "wind_direction_rdegrees", "wind_gust_mps"  )
-
-df = df %>% setnames( old = names_old, new = names_new)
-
+old_names = c(colnames(df))
+new_names = c( "ID", "w_station_name", "elevation", "lat", "long", 
+              "station_num", "city", "province", "observation_date_time", 
+              "observation_date", "year", "month", "day", "hour", "precipitation_last_hr_ml",
+              "air_pressure_hr_hPa", "max_air_pressure_hr_hPa", "min_air_pressure_hr_hPa", 
+              "solar_radiation", "air_temprature", "dew_temp", "max_temp_hr", "max_dew_temp", 
+              "min_temp_hr", "min_dew_temp", "relative_humidity", "max_relative_humidity", 
+              "min_relative_humidity", "wind_speed", "wind_direction_rdegrees", "wind_gust_mps"  )
+df = df %>% setnames( old = old_names, new = new_names)
 colnames(df)
 
 # inspect unique weather station names, 122 as the file suggests
