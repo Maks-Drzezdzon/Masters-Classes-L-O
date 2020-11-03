2d0d4e990ed0955170eb5fb52e9cc1148a3afa52
maksthorn <maksdrzezdzon@gmail.com>
2020-11-03 21:05:29 +0000

    fixing git error

--- assignments/working_w_data/data_wraggling_assignment.R
+++ assignments/working_w_data/data_wraggling_assignment.R
@@ -11,9 +11,9 @@ sao_goncalo = filter(df, w_station_name == "SÃO GONÇALO")
 
 
 
-#################
-# ^^ LOAD ME ^^ #
-#################
+###############
+# ^^ LOAD  ^^ #
+###############
 
 
 
@@ -37,15 +37,15 @@ spec(df)
 
 # rename cols to be more descriptive 
 df = df %>% setnames( 
-                      old = c(colnames(df)), 
-                      new = c( "station_id", "w_station_name", "elevation", "lat", "long", 
-                               "station_num", "city", "province", "observation_date_time", 
-                               "observation_date", "year", "month", "day", "hour", "precipitation_last_hr_ml",
-                               "air_pressure_hr_hPa", "max_air_pressure_hr_hPa", "min_air_pressure_hr_hPa", 
-                               "solar_radiation", "air_temprature", "dew_temp", "max_temp_hr", "max_dew_temp", 
-                               "min_temp_hr", "min_dew_temp", "relative_humidity", "max_relative_humidity", 
-                               "min_relative_humidity", "wind_speed", "wind_direction_rdegrees", "wind_gust_mps")
-                    )
+  old = c(colnames(df)), 
+  new = c( "station_id", "w_station_name", "elevation", "lat", "long", 
+           "station_num", "city", "province", "observation_date_time", 
+           "observation_date", "year", "month", "day", "hour", "precipitation_last_hr_ml",
+           "air_pressure_hr_hPa", "max_air_pressure_hr_hPa", "min_air_pressure_hr_hPa", 
+           "solar_radiation", "air_temprature", "dew_temp", "max_temp_hr", "max_dew_temp", 
+           "min_temp_hr", "min_dew_temp", "relative_humidity", "max_relative_humidity", 
+           "min_relative_humidity", "wind_speed", "wind_direction_rdegrees", "wind_gust_mps")
+)
 
 colnames(df)
 # inspect unique weather station names, 122 as the file suggests
@@ -103,10 +103,6 @@ summary(sao_goncalo$observation_date)
 
 # Look over this
 skim(sao_goncalo)
-# all data
-# plot(df$observation_date_time, df$air_temprature)
-# ToDo look over plots
-# https://www.kaggle.com/sanjayroberts1/exploratory-data-analysis-and-clean-up
 
 plot(sao_goncalo$observation_date_time, sao_goncalo$air_temprature)
 hist( sao_goncalo$observation_date_time, sao_goncalo$air_temprature)
@@ -114,19 +110,46 @@ hist( sao_goncalo$observation_date_time, sao_goncalo$air_temprature)
 # this station has been in operation since 2008 more or less
 # some data missing for 2009-2010, similar blip for 2011-2012
 # there is also lots of 0 values during that time too
+# there is very little data here, probably there was little rainfall
+
+
 plot(sao_goncalo$observation_date_time, sao_goncalo$precipitation_last_hr_ml)
-# there is very little data here meaning there is little rainfall
 plot(sao_goncalo$observation_date_time, sao_goncalo$relative_humidity)
-# another example of missing data in 2009-2010 and 2011-2012
 plot(sao_goncalo$observation_date_time, sao_goncalo$solar_radiation)
-# another example of missing data in 2009-2010 and 2011-2012
-
-identify(sao_goncalo$observation_date_time, sao_goncalo$air_temprature, lables = paste(as.character(sao_goncalo$observation_date_time )))
 
 
-plot(sao_goncalo$observation_date_time, )
+sao_goncalo_2008 = with(sao_goncalo, sao_goncalo[(observation_date_time <= "2008-12-31"), ])
+sao_goncalo_2009 = with(sao_goncalo, sao_goncalo[(observation_date_time <= "2012-12-31" & observation_date_time >= "2012-01-01"), ])
+sao_goncalo_2010 = with(sao_goncalo, sao_goncalo[(observation_date_time <= "2010-12-31" & observation_date_time >= "2010-01-01"), ])
+
+sao_goncalo2 = na.omit(sao_goncalo)
+sao_goncalo2 = with(sao_goncalo2, sao_goncalo2[(observation_date_time <= "2008-12-31"), ])
+
+plot(sao_goncalo2$observation_date_time, sao_goncalo2$precipitation_last_hr_ml)
+plot(sao_goncalo2$observation_date_time, sao_goncalo2$relative_humidity)
+plot(sao_goncalo2$observation_date_time, sao_goncalo2$solar_radiation)
 
 
+ggplot(sao_goncalo2, aes(observation_date_time, precipitation_last_hr_ml)) +
+  ggtitle("title")
+
+ggplot(sao_goncalo_2008) + geom_histogram(bins = 12, aes(x=observation_date_time)) # missing
+ggplot(sao_goncalo_2009) + geom_histogram(bins = 12, aes(x=observation_date_time)) # missing
+ggplot(sao_goncalo_2010) + geom_histogram(bins = 12, aes(x=observation_date_time)) # good year
+
+sao_goncalo_2010 %>% group_by("observation_date_time")
+
+ggplot(data = sao_goncalo_2010) +
+  geom_sf() +
+  xlab("Longitude") + ylab("Latitude") +
+  ggtitle("World map", subtitle = paste0("(", length(unique(sao_goncalo_2010$w_station_name)), " w_station_name)"))
+
+# use lat and log values to create map graph
+# find most rainy months in 2010
+# check lat long values with lat long of city
+# Air pressure and wind analysis
+# graph all stations, compare two that are far apart or close to each other ??
+# move to visualization 
 
 
 
