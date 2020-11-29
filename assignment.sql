488580f4ac46965bab8edcd1a7103ddbd406fb0a
maksthorn <maksdrzezdzon@gmail.com>
2020-11-29 16:39:17 +0000

    adding imgs

--- assignment.sql
+++ assignment.sql
@@ -13,20 +13,28 @@ select max(charges)
 from insurance;
 select min(charges)
 from insurance;
+select median(charges)
+from insurance;
 
 select max(age)
 from insurance;
 select min(age)
+from insurance;
+select median(age)
 from insurance;
 
 select max(children)
 from insurance;
 select min(children)
 from insurance;
+select median(children)
+from insurance;
 
 select max(bmi)
 from insurance;
 select min(bmi)
+from insurance;
+select median(bmi)
 from insurance;
 
 select count(*), smoker
@@ -42,12 +50,15 @@ group by smoker;
 /*274*/
 
 /* 5 number summary */
-select max(charges) "max charges cost"
+select round(max(charges),2) "max charges cost"
 from insurance;
-select min(charges) "min charges cost"
+/*63770.43*/
+select round(min(charges),2) "min charges cost"
 from insurance;
+/*1121.87*/
 select median(charges) "middle charges cost value"
 from insurance;
+/*9382.03*/
 
 select charges
 from ( 
@@ -58,6 +69,7 @@ order by QUARTILE;
 
 select sum(charges)
 from insurance;
+/*17,788,824.99*/
 select round(sum(charges), 2)"all insurance charge costs"
 from (select charges, ntile(4) over(order by charges) as QUARTILE
     from insurance);
@@ -66,16 +78,19 @@ select round(sum(charges), 2)"insurance cost in first quartile"
 from (select charges, ntile(4) over(order by charges) as QUARTILE
     from insurance)
 where quartile = 1;
+/*955784.96*/
 
 select round(sum(charges), 2)"insurance cost in second quartile"
 from (select charges, ntile(4) over(order by charges) as QUARTILE
     from insurance)
 where quartile = 2;
+/*2344668.18*/
 
 select round(sum(charges), 2)"insurance cost in third quartile"
 from (select charges, ntile(4) over(order by charges) as QUARTILE
     from insurance)
 where quartile = 3;
+/*4050700.59*/
 
 select round(sum(charges), 2)"insurance cost in forth quartile"
 from (select charges, ntile(4) over(order by charges) as QUARTILE
@@ -100,5 +115,22 @@ order by charges
 /* std */
 select round(stddev(charges), 2) "Standard Deviation of charges"
 from insurance;
+
+select corr(children, age)
+from insurance;
+/*0.042*/
+select corr(children, charges)
+from insurance;
+/*0.068*/
+select corr(bmi, charges)
+from insurance;
+/*0.198*/
+select corr(age, charges)
+from insurance;
+/*0.299*/
+
+select rank(2344668.18 + 955784.96) within group (order by charges desc) "test"
+from insurance;
+
 
 
