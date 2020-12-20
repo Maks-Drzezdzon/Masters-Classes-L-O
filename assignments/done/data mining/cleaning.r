262bd112d3f85102187bbfa3ead587b25462006a
maksthorn <maksdrzezdzon@gmail.com>
2020-12-20 23:44:17 +0000

    update

--- assignments/done/data mining/cleaning.r
+++ assignments/done/data mining/cleaning.r
@@ -11,7 +11,22 @@ write.csv(df, "bank-full-updated.csv", row.names = F)
 df = read.table("bank/bank-full-updated.csv", header = T, sep = ",")
 head(df)
 unique(df$job , incomparables = FALSE)
+unique(df$marital , incomparables = FALSE)
 unique(df$education , incomparables = FALSE)
+unique(df$default , incomparables = FALSE)
+unique(df$balance , incomparables = FALSE)
+unique(df$housing , incomparables = FALSE)
+unique(df$loan , incomparables = FALSE)
+unique(df$contact , incomparables = FALSE)
+unique(df$day , incomparables = FALSE)
+unique(df$month , incomparables = FALSE)
+unique(df$duration , incomparables = FALSE)
+unique(df$campaign , incomparables = FALSE)
+unique(df$pdays , incomparables = FALSE)
+unique(df$previous , incomparables = FALSE)
+unique(df$poutcome , incomparables = FALSE)
+unique(df$y , incomparables = FALSE)
+
 names(df)[names(df) == "y"] = "subscribed_term_deposit"
 
 df$subscribed_term_deposit[df$subscribed_term_deposit == "no"] = 0 
