85d40ef977002d68cc66adc1d09529cd804f9bdc
maksthorn <maksdrzezdzon@gmail.com>
2020-12-27 12:22:07 +0000

    setting up last project

---
+++ assignments/statistics/PSI_Independent_Project__C15311966.r
@@ -0,0 +1,10 @@
+
+
+library(tidyverse)
+library(data.table)
+library(feather) # file format lib
+library(lubridate) # date transformation 
+library(reticulate) # embed python inside R code
+library(skimr) # summary statistics for larger data sets
+library(forecast)
+
