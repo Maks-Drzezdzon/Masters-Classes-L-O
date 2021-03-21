2b8313b703a3da5c535a847299eeb0f2707b2b99
Maks-drzezdzon <maksdrzezdzon@gmail.com>
2021-03-21 13:34:56 +0000

    setting up assign

---
+++ assignments/machine-learning/ml-assignment.py
@@ -0,0 +1,15 @@
+import tensorflow as tf
+import numpy as np
+import sklearn
+import pandas as pd
+
+from time import time
+import logging
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+from sklearn.datasets import fetch_lfw_people
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.decomposition import PCA
+from sklearn.svm import SVC
