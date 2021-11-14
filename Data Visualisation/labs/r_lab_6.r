00bed8d8e77db4534022cf9bfb26471aeb46b626
djracemix <maksdrzezdzon@gmail.com>
2021-11-14 14:37:28 +0000

    finished lab 7

--- Data Visualisation/labs/r_lab_6.r
+++ Data Visualisation/labs/r_lab_6.r
@@ -13,19 +13,6 @@ df = read.csv("/Users/Grim/Documents/GitHub/Masters-Classes-L-O/Data Visualisati
 
 
 
-# Q1
-test_marks = sqldf('select Subject, Name, round(Mark_Written) as "Mark Written" from df
-group by Name, Subject')
-
-hist(test_marks$`Mark Written`, 
-     col="skyblue2", 
-     xlab="Mark Ranges for Students",
-     main="")
-
-# flip bar chart orientation
-qplot(test_marks$`Mark Written`, geom="histogram", xlab="Mark Ranges for Students") + coord_flip()
-
-
 
 # lab 8
 df = read.csv("/Users/Grim/Documents/GitHub/Masters-Classes-L-O/Data Visualisation/data/studentresult.csv", sep= ',' , header=T )
