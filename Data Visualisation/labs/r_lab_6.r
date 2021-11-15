fbce3b41636a2ef942f9d4bc30808035895da3c6
Maks-drzezdzon <maksdrzezdzon@gmail.com>
2021-11-15 20:28:58 +0000

    setting up final lab

--- Data Visualisation/labs/r_lab_6.r
+++ Data Visualisation/labs/r_lab_6.r
@@ -11,7 +11,7 @@ library(maps)
 
 # lab 8
 df = read.csv("/Users/Grim/Documents/GitHub/Masters-Classes-L-O/Data Visualisation/data/OlympicGames.csv", sep= ',' , header=T )
-
+df = read.csv("/Users/maksdrzezdzon/Documents/GitHub/Masters-Classes-L-O/Data Visualisation/data/OlympicGames.csv", sep = ",", header = T)
 df$gold = ifelse(is.na(df$gold), 0, df$gold)
 df$silver = ifelse(is.na(df$silver), 0, df$silver)
 df$bronze = ifelse(is.na(df$bronze), 0, df$bronze)
@@ -29,50 +29,74 @@ df
 # 4) Who has the most total medals? (2 marks)
 # 5) What is the variation and spread of ages amongst gold and silver medalists? (2 marks)
 
-
-# 1)
+mapdata = map_data("world") %>% as_tibble()
+mapdata
+# 1) What country has won most Silver medals since 2000?
 df_year_2000 = with(df, df[(Year >= 2000),])
-df_year_2000 = with(df_year_2000, df_year_2000[(Medal == "silver"),])
 
-df_year_2000
-test_marks_2 = sqldf('select DOB, Subject, Name, round(avg(Mark_Written) + avg(Mark_Oral), 1)/2 as Test_mark from df group by Name, Subject')
-plot = ggplot(data = df_year_2000, aes(Medal, Year, fill=as.factor(Country))) +
- geom_col(position="dodge") + labs(x="Silver Medals Won", y="Silver Medals Count",
-             title="Q8-1 What country has won most Silver medals since 2000?", fill="Country Name")
-plot
-df_year_2000
-ggplot(df_year_2000, aes(map_id = Country)) + geom_map(aes(fill = Medal), map = df_year_2000)
+df_year_2000_tmp = sqldf('select Year, count(Medal) as Medal, Country from df_year_2000 where Medal == "silver" group by Country')
+names(df_year_2000_tmp)[3] = "region"
+df_year_2000_tmp
+silver_medals = right_join(mapdata, df_year_2000_tmp, by="region")
+silver_medals
 
-+ expand_limits(x = states_map$long, y = states_map$lat) + facet_wrap( ~ variable)
+map1 = ggplot() +
+  geom_map(aes(lat, long, map_id = Medal), map=silver_medals, color = "grey80", fill="grey30", size = 0.3)
+map1
 
 
+map1 = ggplot(silver_medals, aes( x = long, y = lat, group=group)) +
+  geom_polygon(aes(fill = Medal), color = "black")
+
+map1
+map2 = map1 + scale_fill_gradient(name = "Total Medals Won", low = "white", high = "blue", na.value = "grey50")+
+  theme(axis.text.x = element_blank(),
+        axis.text.y = element_blank(),
+        axis.ticks = element_blank(),
+        axis.title.y=element_blank(),
+        axis.title.x=element_blank(),
+        rect = element_blank())
+map2
 
 
-# 2) 
+# 2) How is the gender balance amongst the United States gold medalist?
 df_usa = with(df, df[(Country == "USA"),])
 df_usa = with(df_usa, df_usa[(Medal == "gold"),])
-df_usa
 
-plot = ggplot(data = df_usa, aes(fill=as.factor(Gender))) +
-geom_bar(x=Gender, position="dodge") + labs(x="Student Name", y="Grade Average", 
-                                    title="Q8-2 How is the gender balance amongst the United States gold medalist?", fill="Subjects")
+df_usa_tmp = sqldf('select Gender, Medal, Country from df_usa')
+df_usa_tmp
+
+
+# 3) What are the best sports for Sweden, USA, Austria and Switzerland?
+
+df_countires_list = with(df, df[(Country == c("USA", "Sweden", "Austria", "Switzerland")),])
+df_countires_list
+df_countires_list = with(df_usa, df_usa[(Medal == "gold"),])
+
+df_countires_list_tmp = sqldf('select Gender, Medal, Country from df_countires_list')
+df_countires_list_tmp
+
+
+# 4) Who has the most total medals?
+
+medals_total = sqldf('select count(Medal) as Medal_Count, Country from df group by Country')
+medals_total
+
+plot = ggplot(data = medals_total, aes(Country, Medal_Count, fill=as.factor(Country))) +
+  geom_col(position="dodge") + 
+  labs(x="Country Name", y="Medals ", title="Who has the most total medals?", fill="Countries")
+
 plot
 
-ggplot (data=df_usa, fill=Gender) +
-  geom_bar() + stat_count(x=df_usa$Gender)
 
-# 3)
+# 5) What is the variation and spread of ages amongst gold and silver medalists?
+df
 
-
-
-
-# 4)
+medal_age = sqldf('select Athlete_Age, Medal from df where Medal != "bronze"')
+medal_age = medal_age %>% drop_na()
+medal_age
 
 
-
-
-
-# 5)
 
 
 
